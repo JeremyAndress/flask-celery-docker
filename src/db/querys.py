ffb98f84672cb2628ffed079fb5a97415e49c427
JeremyAndress <jeremy.a.silva.silva@gmail.com>
2020-07-23 20:17:31 -0400

    :lock: remove tables

--- src/db/querys.py
+++ src/db/querys.py
@@ -1,23 +1,7 @@
-#QUERYS ALTO VALOR
+#EXAMPLES QUERY
 
-def insert_notif(canal,msisdn,fr,id_event,monto):
-    return f"""INSERT INTO public.notificaciones
-            (canal, msisdn, fecha_registro, id_evento_id, monto)
-            VALUES('{canal}', '{msisdn}', '{fr}', {id_event}, {monto});"""
+def insert_notif(channel,number,date):
+    return f"""INSERT INTO public.table
+            (channel, number, date)
+            VALUES('{channel}', '{number}', '{date}' );"""
 
-#QUERYS SCE PROYECTOS
-
-def insert_tbl(msisdn, ini_fec,ini_estado,now_fec,now_estado,now_monto):
-    return f""" INSERT INTO sce_proyectos.tbl_altovalor_estado
-            (msisdn, ini_fec, ini_estado, now_fec, now_estado, now_monto)
-            VALUES('{msisdn}', '{ini_fec}', {ini_estado}, '{now_fec}', {now_estado}, {now_monto}); """
-
-
-def delete_tbl(id):
-    return f""" DELETE FROM sce_proyectos.tbl_altovalor_estado
-            WHERE id='{id}'; """
-
-def select_tbl(msisdn):
-    return f""" SELECT id, msisdn, ini_fec, ini_estado, now_fec, now_estado, now_monto
-            FROM sce_proyectos.tbl_altovalor_estado 
-            where msisdn='{msisdn}'; """
